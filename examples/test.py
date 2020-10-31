bc3ee269311aabf3a66c444481389a0f350cc99e
Piotr Tutak <p.tutak@outlook.com>
2020-10-31 01:28:35 +0100

    crypto testing

---
+++ examples/test.py
@@ -0,0 +1,37 @@
+import hashlib
+import pickle
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.fernet import Fernet
+
+private_key = rsa.generate_private_key(65537, key_size=2048, backend=default_backend())
+publicKey = private_key.public_key()
+
+
+encrypted = publicKey.encrypt(
+    b"encrypt me",
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None,
+    )
+)
+
+sendKey = publicKey.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+)
+
+print(sendKey)
+print(len(sendKey))
+
+sendKeySha256 = hashlib.sha256(sendKey).hexdigest()
+print(sendKeySha256)
+print(len(sendKeySha256))
+payload = pickle.dumps((sendKey, sendKeySha256))
+print(payload)
+print(len(payload))
