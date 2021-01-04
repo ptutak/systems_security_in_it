3b80d2b9b13d03d7611ceb7a45a2d72933fa29f4
Piotr Tutak <p.tutak@outlook.com>
2021-01-04 21:24:05 +0100

    gui client

---
+++ encrypted_comm/gui_client.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from typing import Optional
+from .client import Client, Observer, ObserverCreator
+
+
+class GuiClient(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self._client: Optional[Client] = None
+
+    def initialize_client(self, server_address):
+        self._client = Client(server_address, ObserverCreator())
